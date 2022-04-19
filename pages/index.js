bf8385099cec299afcc01d43f0ce0beaadc2761e
nikhiljerry29 <nikhil.gupta5667@gmail.com>
2022-04-20 00:37:13 +0530

    Add Navbar Component

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Header from "../components/Header";
+import Navbar from "../components/Navbar";
 
 export default function Home() {
     return (
@@ -8,7 +9,7 @@ export default function Home() {
                 <title>Hulu Clone</title>
             </Head>
             <Header />
-            {/* Navbar */}
+            <Navbar />
             {/* Results */}
         </>
     );
